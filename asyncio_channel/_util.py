79b78184378843b3decfa5e29bed47907db1df11
comex <comexk@gmail.com>
2020-08-17 15:51:00 -0700

    After cancelling tasks, wait for them to finish rather than immediately dropping them.
    This prevents asyncio from complaining about pending tasks being
    dropped.

--- asyncio_channel/_util.py
+++ asyncio_channel/_util.py
@@ -1,15 +1,35 @@
 __all__ = ('wait_all', 'wait_first')
 
-from asyncio import FIRST_COMPLETED, CancelledError, Task, create_task, wait
+from asyncio import FIRST_COMPLETED, CancelledError, Task, create_task, wait, get_running_loop
+import functools
 
+async def cancel_and_wait(task,
+    get_running_loop=get_running_loop):
+    """Cancel the task and wait until it completes.
+    Based on asyncio._cancel_and_wait.
+    """
+    loop = get_running_loop()
+    waiter = loop.create_future()
+    # Use a done callback instead of await, because otherwise we would be
+    # unable to distinguish the CancelledError from the task from a
+    # CancelledError we might receive due to being cancelled ourselves.
+    def release(_):
+        if not waiter.done():
+            waiter.set_result(None)
+    task.add_done_callback(release)
+
+    try:
+        task.cancel()
+        await waiter
+    finally:
+        task.remove_done_callback(release)
 
 async def wait_all(
         *coros_or_tasks,
         timeout=None,
         create_task=create_task,
         wait=wait,
-        CancelledError=CancelledError,
-        Task=Task):
+        cancel_and_wait=cancel_and_wait):
     """Wait for all tasks to complete.
 
     Returns two tuples: completed tasks, pending tasks.  If timeout is
@@ -26,12 +46,12 @@ async def wait_all(
     except CancelledError:
         for task in tasks:
             if not task.done():
-                task.cancel()
+                await cancel_and_wait(task)
 
         raise
     else:
         for task in pending:
-            task.cancel()
+            await cancel_and_wait(task)
 
         return done, pending
 
@@ -41,7 +61,8 @@ async def wait_first(
         timeout=None,
         wait=wait,
         return_when=FIRST_COMPLETED,
-        CancelledError=CancelledError):
+        CancelledError=CancelledError,
+        cancel_and_wait=cancel_and_wait):
     """Wait for first task to complete.
 
     Returns two tuples: completed tasks, pending tasks.  If timeout is
@@ -66,6 +87,6 @@ async def wait_first(
         raise
     else:
         for task in pending:
-            task.cancel()
+            await cancel_and_wait(task)
 
         return done, pending
